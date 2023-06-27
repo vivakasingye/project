070a8807a847cf88c15ef0fcd1028602943f8f2a
vivakasingye <56263871+vivakasingye@users.noreply.github.com>
2023-06-27 11:41:19 +0300

    bg

--- includes/header.php
+++ includes/header.php
@@ -20,6 +20,7 @@
 <link rel="stylesheet" href="assets/css/odometer.min.css">
 <link rel="stylesheet" href="assets/css/style.css">
 <link rel="stylesheet" href="assets/css/responsive.css">
+<link rel="shortcut icon" href="assets/images/fav-icon.png" type="image/png">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
